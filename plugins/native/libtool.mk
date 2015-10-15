34cc4006953c6bb06f55acd21c35af9711df4cd9
Tony Theodore <tonyt@logyst.com>
2015-10-15 21:12:17 +1100

    add native and apps plugin packages

---
+++ plugins/native/libtool.mk
@@ -0,0 +1,29 @@
+# This file is part of MXE.
+# See index.html for further information.
+
+PKG             := libtool
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 2.4.4
+$(PKG)_CHECKSUM := 159d4e20c201f929e3562536d3ae6b5e605403fa4bb4e72ef197a4e162c3fedf
+$(PKG)_SUBDIR   := $(PKG)-$($(PKG)_VERSION)
+$(PKG)_FILE     := $(PKG)-$($(PKG)_VERSION).tar.gz
+$(PKG)_URL      := http://ftp.gnu.org/gnu/$(PKG)/$($(PKG)_FILE)
+$(PKG)_WEBSITE  := https://www.gnu.org/software/libtool
+$(PKG)_MAKEFILE := $(lastword $(MAKEFILE_LIST))
+$(PKG)_OWNER    := https://github.com/tonytheodore
+$(PKG)_TARGETS  := $(BUILD)
+$(PKG)_DEPS     := autoconf automake
+
+define $(PKG)_UPDATE
+    $(WGET) -q -O- 'http://ftp.gnu.org/gnu/libtool/?C=M;O=D' | \
+    $(SED) -n 's,.*<a href="libtool-\([0-9][^"]*\)\.tar.*,\1,p' | \
+    head -1
+endef
+
+define $(PKG)_BUILD_$(BUILD)
+    mkdir '$(1).build'
+    cd    '$(1).build' && '$(1)/configure' \
+        --prefix='$(PREFIX)/$(TARGET)'
+    $(MAKE) -C '$(1).build' -j '$(JOBS)'
+    $(MAKE) -C '$(1).build' -j 1 install
+endef
