0320cb33fe899ed3ae41a01622966e988b2bab7e
Dustin Counsell <dcounsell@mitre.org>
2015-11-19 22:45:03 -0500

    updated gdal package version and checksum

--- src/gdal.mk
+++ src/gdal.mk
@@ -3,8 +3,8 @@
 
 PKG             := gdal
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 1.10.1
-$(PKG)_CHECKSUM := b4df76e2c0854625d2bedce70cc1eaf4205594ae
+$(PKG)_VERSION  := 2.0.1
+$(PKG)_CHECKSUM := ac82273d81f340e0d43d6d7715cfd256f0cf1e32
 $(PKG)_SUBDIR   := gdal-$($(PKG)_VERSION)
 $(PKG)_FILE     := gdal-$($(PKG)_VERSION).tar.gz
 $(PKG)_URL      := http://download.osgeo.org/gdal/CURRENT/$($(PKG)_FILE)
